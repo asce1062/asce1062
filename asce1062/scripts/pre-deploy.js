c74e8a8fdcbc943d4f0f4bf09823a64093bb4e9c
asce1062 <tnkratos@gmail.com>
2025-06-14 15:07:27 +0300

    Feat: artifacts and deployment to separate branch   - use gh-pages library to automate push and build   - add .nojekyll to prevent skipping `_asset` folders   - rename _astro to astro - remove _config.yml from root directory - update pre-deploy and deploy commands - rename post-build scripts

--- asce1062/scripts/pre-deploy.js
+++ asce1062/scripts/pre-deploy.js
@@ -1,21 +1,27 @@
+/**
+ * this scripts helps us automate some fixes to allows us to deploy our site with github pages
+ * leading slashes break path leading to assets not being accessed correctly.
+ * static files are generated a level above css files nested in folders.
+ *
+ */
+
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const distDir = path.join(__dirname, '../dist');
-const artifactsDir = path.join(distDir, '../../artifacts');
 
 /**
  *
- * remove leading forward slash to linked generated stylesheets
- * replace href="/_astro/index..." > href="_astro/index..."
+ * remove leading forward slash and underscore to linked generated stylesheets
+ * replace href="/_astro/index..." > href="astro/index..."
  *
  */
 function fixHtmlFile(filePath) {
   let content = fs.readFileSync(filePath, 'utf-8');
   const original = content;
-  content = content.replace(/href="\/(_astro\/[^"]+\.css)"/g, 'href="$1"');
+  content = content.replace(/href="\/_(astro\/[^"]+\.css)"/g, 'href="$1"');
 
   if (content !== original) {
     console.log(`✅ Fixed href path in HTML: ${filePath}`);
@@ -53,41 +59,7 @@ function walkAndFix(dir) {
   }
 }
 
-function removeDirRecursively(dirPath) {
-  if (fs.existsSync(dirPath)) {
-    fs.rmSync(dirPath, { recursive: true, force: true });
-    console.log(`🗑️ Removed existing directory: ${dirPath}`);
-  }
-}
-
-/**
- *
- * cheeky workaround to have our sources and site hosted on github
- * move static files /dist > ../../artifacts
- *
- */
-
-function copyDirRecursively(src, dest) {
-  if (!fs.existsSync(dest)) {
-    fs.mkdirSync(dest, { recursive: true });
-  }
-
-  for (const item of fs.readdirSync(src)) {
-    const srcPath = path.join(src, item);
-    const destPath = path.join(dest, item);
-
-    if (fs.statSync(srcPath).isDirectory()) {
-      copyDirRecursively(srcPath, destPath);
-    } else {
-      fs.copyFileSync(srcPath, destPath);
-    }
-  }
-  console.log(`📁 Copied dist → ${dest}`);
-}
-
 // Main
-console.log('🔧 Running post-build fix and copy...');
+console.log('🔧 Running pre-deploy fixes...');
 walkAndFix(distDir);
-// removeDirRecursively(docsDir);
-// copyDirRecursively(distDir, docsDir);
 console.log('🎉 All done!');
