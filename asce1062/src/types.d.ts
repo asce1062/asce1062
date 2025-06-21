e2bb22082f6edab7f560074ec0723db7e80912c5
asce1062 <tnkratos@gmail.com>
2025-06-21 11:40:45 +0300

    update tag descriptions update blog   - update time image   - add about trusting the process update timeline components   - add typing

--- asce1062/src/types.d.ts
+++ asce1062/src/types.d.ts
@@ -10,3 +10,14 @@ export interface Posts {
         tags: string[];
     };
 }
+
+export interface TimelineEntry {
+    title: string;
+    date: string;
+    desc: string;
+    desc1?: string;
+    desc2?: string;
+    desc3?: string;
+    desc4?: string;
+    desc5?: string;
+}
