f2d171b9d61e88fd47d040ae671b691fed224e4b
asce1062 <tnkratos@gmail.com>
2025-08-11 17:11:57 +0300

    fix:   - links to part 2 icomoon workflow   - open-graph image generation font

--- asce1062/src/pages/open-graph/[...route].ts
+++ asce1062/src/pages/open-graph/[...route].ts
@@ -10,7 +10,16 @@ export const { getStaticPaths, GET } = OGImageRoute({
     description: page.frontmatter.description,
     logo: { path: 'public/icon.png' },
     bgGradient: [[19, 15, 25]],
-    font: { title: { families: [ "0xProto", "monospace" ] }, description: { families: [ "0xProto", "monospace" ] } },
+    font: { 
+      title: { 
+        families: [ "0xProto", "monospace" ], 
+        size: 48 
+      }, 
+      description: { 
+        families: [ "0xProto", "monospace" ], 
+        size: 24 
+      } 
+    },
     fonts: ["public/fonts/0xProto/0xProtoNerdFont-Regular.ttf"]
   }),
 });
